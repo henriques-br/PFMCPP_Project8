cafd1dc22c8bc07f087046f482b24e3cb53ad9f3
Alex Henriques <henriquesbr@gmail.com>
2026-06-10 15:42:15 -0300

    implemented Highway

--- Highway.cpp
+++ Highway.cpp
@@ -1,6 +1,8 @@
-#include "Highway.h"
-
 #include <cassert>
+#include "Car.h"
+#include "Motorcycle.h"
+#include "SemiTruck.h"
+#include "Highway.h"
 
 void Highway::changeSpeed(int newSpeed)
 {
@@ -13,23 +15,47 @@ void Highway::changeSpeed(int newSpeed)
 
 void Highway::addVehicleInternal(Vehicle* v)
 {
-    assert(false);
+    //assert(false);
 
     /*
     depending on the derived type, call the member function that doesn't evade the cops. 
     do not call `setSpeed`.  Pick a different function.
     */
+    if( auto* car = dynamic_cast<Car*>(v) )
+    {
+        car->closeWindows();
+    }  
+    else if( auto* bike = dynamic_cast<Motorcycle*>(v) ) 
+    {
+        bike->lanesplitAndRace(100);
+    }
+    else if ( auto* truck = dynamic_cast<SemiTruck*>(v) )
+    {
+        truck->enterHighway();
+    }
 }
 
 void Highway::removeVehicleInternal(Vehicle* v)
 {
-    assert(false);
+    //assert(false);
 
     /*
     depending on the derived type, call the member function that tries to evade the cops. 
     do not call `setSpeed`.  Pick a different function.
     trucks pull over, but cars and bikes try to evade!!
     */
+    if( auto* car = dynamic_cast<Car*>(v) ) 
+    {
+        car->tryToEvade();
+    } 
+    else if( auto* bike = dynamic_cast<Motorcycle*>(v) ) 
+    {
+        bike->tryToEvade();
+    }
+    else if( auto* truck = dynamic_cast<SemiTruck*>(v) ) 
+    {
+        truck->pullOver();
+    }
 }
 
 void Highway::addVehicle(Vehicle* v)
@@ -37,6 +63,7 @@ void Highway::addVehicle(Vehicle* v)
     vehicles.push_back(v);
     addVehicleInternal(v);
 }
+
 void Highway::removeVehicle(Vehicle* v)
 {
     vehicles.erase(std::remove(vehicles.begin(), 
