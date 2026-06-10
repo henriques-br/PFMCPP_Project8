a6ad9943dc2f36251ae4b377822899ceecd99fcc
Alex Henriques <henriquesbr@gmail.com>
2026-06-10 15:42:46 -0300

    implemented Motorcycle

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -1,12 +1,14 @@
 #include "Motorcycle.h"
 
 Motorcycle::Motorcycle(const std::string& n) : Vehicle(n) {}
+Motorcycle::~Motorcycle() = default;
 
 void Motorcycle::lanesplitAndRace( int topSpeed )
 {
     setSpeed(topSpeed);
     std::cout << name << (topSpeed > 90 ? ": yeeehawwww" : ": zzzzzzzz") << std::endl;
 }
+
 void Motorcycle::tryToEvade()
 {
     std::cout << name << ": you'll never take me alive, ya dirty coppers!" << std::endl;
